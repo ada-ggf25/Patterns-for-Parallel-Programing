ccfb46c6807684286a527af4842854f9db11cbfb
Guilherme Grancho <guilherme.fernandes25@imperial.ac.uk>
2026-05-01 19:26:01 +0100

    Removed ignore for Assessment 1

---
+++ Assessment 1/build/CMakeFiles/integrate.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/integrate.dir/integrate.cpp.o"
+  "CMakeFiles/integrate.dir/integrate.cpp.o.d"
+  "CMakeFiles/integrate.dir/integrate_serial.cpp.o"
+  "CMakeFiles/integrate.dir/integrate_serial.cpp.o.d"
+  "integrate"
+  "integrate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/integrate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
